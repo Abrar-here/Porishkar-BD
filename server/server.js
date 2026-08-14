66788212cc21701aa73c08615f86301f050a76a0
Ahsanul <ahsanulfahim2002@gmail.com>
2026-08-15 01:31:11 +0600

    Save F09 marketplace changes before syncing team branch

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 
+import dns from "node:dns";
 import "./src/config/env.js";
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
 
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const PORT = process.env.PORT || 5000;
 
